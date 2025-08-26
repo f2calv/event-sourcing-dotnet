9357dc87c4ff35b19e2c80723e62eb374ac6c793
Alex Vincent <alex@far2cool.com>
2025-08-27 01:36:20 +0200

    F2calv/2024 11 updates (#10)
    * update devcontainer
    
    * bunch of updates

--- src/MartenApp/Services/MyBgService.cs
+++ src/MartenApp/Services/MyBgService.cs
@@ -1,13 +1,11 @@
 ﻿using Marten;
-using Marten.Events;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace CasCap.Services;
 
 public class MyBgService : BackgroundService
 {
-    readonly ILogger _logger;
-    readonly IDocumentStore _store;
+    private readonly ILogger _logger;
+    private readonly IDocumentStore _store;
 
     public MyBgService(ILogger<MyBgService> logger, IDocumentStore store)
     {
@@ -20,15 +18,14 @@ public class MyBgService : BackgroundService
         _logger.LogInformation("starting...");
         await Task.Delay(0, stoppingToken);
 
-
-        await using (var session = _store.OpenSession())
+        await using (var session = _store.OpenSession(new Marten.Services.SessionOptions() { }))
         {
             // Start a brand new stream and commit the new events as part of a transaction
             //session.Events.StartStream<Quest>(questId, started, joined1);
 
             // Start a brand new stream
             var stream_id_location = Guid.NewGuid();
-            while (true)
+            while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("iterating...");
                 await Task.Delay(1_000, stoppingToken);
@@ -40,7 +37,7 @@ public class MyBgService : BackgroundService
                     session.Events.Append(stream_id_location, new TemperatureLogged(DateTime.Now, i));
                     // Save the pending changes to db
                     await session.SaveChangesAsync();
-                    _logger.LogInformation($"added event to stream {stream_id_location}");
+                    _logger.LogInformation("added event to stream {StreamIdLocation}", stream_id_location);
 
                     await Task.Delay(1_000, stoppingToken);
 
